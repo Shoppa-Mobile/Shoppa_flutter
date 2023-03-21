b94c6ebb89673ebee1c5281ce1566969131b56f3
Fortune <fortune.onyema22@gmail.com>
2023-03-21 21:22:31 +0100

    Added Loading widget file and continued refactoring of shop screens

---
+++ lib/widgets/loading.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:loading_overlay/loading_overlay.dart';
+import 'package:shoppa_app/constants/colors.dart';
+import 'package:shoppa_app/constants/constants.dart';
+
+// ignore: must_be_immutable
+class Loading extends StatefulWidget {
+  bool isLoading;
+  final String? text;
+  final Widget child;
+  Loading({
+    Key? key,
+    required this.child,
+    this.text,
+    required this.isLoading,
+  }) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return LoadingOverlay(
+      isLoading: widget.isLoading,
+      color: primaryColor,
+      opacity: 0.3,
+      progressIndicator: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(
+              color: primaryColor,
+              strokeWidth: 7.0,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              widget.text!,
+              style: headerStyle3,
+            )
+          ],
+        ),
+      ),
+      child: widget.child,
+    );
+  }
+}
